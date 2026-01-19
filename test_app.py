4d29aafaad23be406cbbd1c5316ab0d95b6292e5
Your Name <your-email@example.com>
2026-01-18 19:21:53 -0800

    Add .gitignore and remove pycache

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 from app import app
 
+<<<<<<< HEAD
 def test_home_route():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+=======
+def test_home_status_code():
+    client = app.test_client()
+    resp = client.get("/")
+    assert resp.status_code == 200
+>>>>>>> 5770a1f (Add requirements and basic test)
